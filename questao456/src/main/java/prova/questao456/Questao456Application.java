a18f9cdeaf6fcad00c065641022ceea147caa30c
DenisMarx <denisdiego_kls@hotmail.com>
2018-10-08 21:20:57 -0300

    versão1.0

---
+++ questao456/src/main/java/prova/questao456/Questao456Application.java
@@ -0,0 +1,12 @@
+package prova.questao456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Questao456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Questao456Application.class, args);
+	}
+}
